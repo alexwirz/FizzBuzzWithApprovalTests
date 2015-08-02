0ea2a71f4c9ec5043b7bef30a815f79455e65db5
Alexander Wirz <alex.wirz@gmail.com>
2015-08-02 16:15:22 +0200

    Add test for all combinations.

--- FizzBuzz/FizzBuzzTests.cs
+++ FizzBuzz/FizzBuzzTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ApprovalTests.Reporters;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,20 @@ using System.Threading.Tasks;
 
 namespace FizzBuzz
 {
+    [UseReporter(typeof(DiffReporter))]
+    [TestFixture]
     public class FizzBuzzTests
     {
+        [Test]
+        public void TestAllFizzBuzzCombinations()
+        {
+            var numbers = Enumerable.Range(1, 99).Select(number => number.ToString());
+            ApprovalTests.Combinations.CombinationApprovals.VerifyAllCombinations(FizzBuzz, numbers);
+        }
+
+        private string FizzBuzz(string number)
+        {
+            return number;
+        }
     }
 }
